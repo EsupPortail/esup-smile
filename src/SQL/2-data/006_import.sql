fb74aea22815147100d84a447d13e437dd558fa3
smile-unicaen <139756180+smile-unicaen@users.noreply.github.com>
2023-09-25 09:56:43 +0200

    Delete src/SQL directory

--- src/SQL/2-data/006_import.sql
+++
@@ -1,8 +0,0 @@
----------- Data
-INSERT INTO source (code, libelle, importable)
-VALUES ('smile', 'SMILE', false), ('pyc', 'Pick-Your-Courses', true)
-    ON CONFLICT (code)
-DO UPDATE SET
-    libelle=excluded.libelle,
-    importable=excluded.importable;
-
